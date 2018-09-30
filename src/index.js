5fa3bd62c1a87a875e3fb060038fc30e71ce81e1
fyr-fyrrr <llenasakalova@gmail.com>
2018-09-30 11:58:05 +0300

    it's not ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let a = 0;
+  let b = 0;
+	 for (i = 0; i <str.length; i++) {
+		  if  (str.charAt(i) == '(') {
+			  a++
+		  }
+		  else {
+			  a-- 
+		  };
+		  if (a < 0) {
+			  return (false);
+			  b++
+		  };
+	  }
+	 if (b == 0) {
+		  return (true);
+	 };
 }
